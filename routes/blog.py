21824d44f325d3d9af12011239cb2181d06a3277
valentinchevaux <37655913-valentinchevaux@users.noreply.replit.com>
2024-12-16 18:20:01 +0000

    Agent query: Est-ce que la page blog affiche correctement le message "en construction" et le bouton vers l'e-book ?
    Refactor blog page: simplify structure and remove sector-based article grouping.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/3f77f9f6-9ecc-4653-998e-1f61b4ba6d7f/90469338-757c-4db5-a07e-f698ffb09671.jpg

--- routes/blog.py
+++ routes/blog.py
@@ -1,30 +1,8 @@
-from flask import Blueprint, render_template, abort
-from models.blog import BlogPost
-from sqlalchemy import desc
+from flask import Blueprint, render_template
 
 blog = Blueprint('blog', __name__)
 
 @blog.route('/blog')
 def blog_index():
-    """Page principale du blog avec liste des articles"""
-    posts = BlogPost.query.order_by(desc(BlogPost.created_at)).all()
-    # Grouper les articles par secteur
-    posts_by_sector = {}
-    for post in posts:
-        if post.sector not in posts_by_sector:
-            posts_by_sector[post.sector] = []
-        posts_by_sector[post.sector].append(post)
-    
-    return render_template('blog/index.html', posts_by_sector=posts_by_sector)
-
-@blog.route('/blog/<string:slug>')
-def blog_post(slug):
-    """Page d'un article individuel"""
-    post = BlogPost.query.filter_by(slug=slug).first_or_404()
-    # Trouver des articles reliés dans le même secteur
-    related_posts = BlogPost.query.filter(
-        BlogPost.sector == post.sector,
-        BlogPost.id != post.id
-    ).limit(3).all()
-    
-    return render_template('blog/post.html', post=post, related_posts=related_posts)
+    """Page du blog (en construction)"""
+    return render_template('blog.html')
